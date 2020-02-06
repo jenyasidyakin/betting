2a3d705bb2f554eaeaa13a2cdea4f615ebc23121
Jenya Sidyakin <jenya.sidyakin@gmail.com>
2020-02-06 20:01:16 +0200

    message

--- server.js
+++ server.js
@@ -15,21 +15,26 @@ app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
 
 app.get('/',function(req,res){
+	console.log("get /");
 	res.json({'status':200})
 })
 
 app.get('/key1',function(req,res){
+	console.log("get key1");
 	res.json({"status":200});
 })
 app.get('/key2',function(req,res){
+	console.log("get key2");
 	res.json({"status":200});
 })
 
 app.post('/key1',function(req,res){
+	    console.log("post key1");
 		console.log(req.body);
 		res.json({'status':200})
 })
 app.post('/key2',function(req,res){
+		console.log("post key2");
 		console.log(req.body);
 		res.json({'status':200})
 })
